adbe2e62992db74e6dea8109e8d7bca9fb33fd72
Bhavya <xbhavyatomar@gmail.com>
2021-10-10 15:59:52 +0530

    setup auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
+import FirebaseAuthService from "./FirebaseAuthService";
 import "./App.scss";
 // eslint-disable-next-line no-unused-vars
 import firebase from "./FirebaseConfig";
 
+import LoginForm from "./components/LoginForm";
+
 function App() {
+  const [user, setUser] = useState(null);
+
+  FirebaseAuthService.subscribeToAuthChanges(setUser);
   return (
     <div className="App">
       <div className="title-row">
         <h1 className="title">Firebase Recipies</h1>
+        <LoginForm existingUser={user}></LoginForm>
       </div>
     </div>
   );
---
+++ src/components/LoginForm.js
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import FirebaseAuthService from "../FirebaseAuthService";
+
+function LoginForm({ existingUser }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      await FirebaseAuthService.loginUser(username, password);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  function handleLogout() {
+    FirebaseAuthService.logoutUser();
+  }
+
+  async function handleSendResetPasswordEmail() {
+    if (!username) {
+      alert("missing username");
+      return;
+    }
+    try {
+      await FirebaseAuthService.sendPasswordResetEmail(username);
+      alert("sent the password reset email");
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  // async function handleLoginWithGoogle() {
+  //   try {
+  //     await FirebaseAuthService.loginWithGoogle();
+  //   } catch (error) {
+  //     alert(error.message);
+  //   }
+  // }
+
+  return (
+    <div className="login-form-container">
+      {existingUser ? (
+        <div className="row">
+          <h3>Welcome, {existingUser.email}</h3>
+          <button
+            type="button"
+            className="primary-button"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit} className="login-form">
+          <label className="input-label login-label">
+            Username (email):
+            <input
+              type="email"
+              required
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="input-text"
+            ></input>
+          </label>
+          <label className="input-label login-label">
+            Password:
+            <input
+              type="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input-text"
+            ></input>
+          </label>
+          <div className="button-box">
+            <button className="primary-button">Login</button>
+            <button type="button" onClick={handleSendResetPasswordEmail}>
+              Reset Password
+            </button>
+            {/* <button
+              type="button"
+              className="primary-button"
+              onClick={handleLoginWithGoogle}
+            >
+              Login with Google
+            </button> */}
+          </div>
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default LoginForm;
